9f7cfd8f2e32121a2747105b0a2a03e35fbfc15f
Anthony_Chiodi <adc8wlc@gmail.com>
2023-11-22 14:44:10 -0500

    add filter for valid phone number

--- transform/restaurant_validation.py
+++ transform/restaurant_validation.py
@@ -1,11 +1,15 @@
 import re
 import pandas as pd
 import pdb
+from user_validation import is_valid_phone_number, validate_column
+
 
 def restaurant_validation(df, sheet_name):
-    pdb.set_trace()
     not_null_df = df.copy()
 
 # Exclude rows where any column (except 'menu' and 'description') has null values
     not_null_df = not_null_df.dropna(subset=not_null_df.columns.difference(['menu', 'description']))
-    return(not_null_df, sheet_name)
+    validate_column(not_null_df, 'phone_number', is_valid_phone_number, 'is_valid_phone', sheet_name)
+    clean_df = not_null_df.loc[(not_null_df['is_valid_phone'] != False)]
+
+    return(clean_df, sheet_name)
